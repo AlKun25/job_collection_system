d8009d8e2647572fd1fb693887596208fb03a107
Kunal <mundadakunal25@gmail.com>
2025-03-18 21:13:39 -0700

    Add non-singleton class for database connections

--- dags/job_utils/db/manager.py
+++ dags/job_utils/db/manager.py
@@ -25,8 +25,11 @@ class DBManager:
     def get(self, model, obj_id: int):
         return self._session.query(model).get(obj_id)
 
-    def get_by_filter(self, model, **kwargs):
-        return self._session.query(model).filter_by(**kwargs).all()
+    def get_by_filter(self, model, return_all: bool = False, **kwargs):
+        if return_all:
+            return self._session.query(model).filter_by(**kwargs).all()
+        else:
+            return self._session.query(model).filter_by(**kwargs).first()
 
     def update(self, obj_id: int, model, **kwargs):
         obj = self.get(model, obj_id)
@@ -42,3 +45,45 @@ class DBManager:
 
     def close(self):
         self._session.close()
+
+class DBContext:
+    def __init__(self, connection_url):
+        self.connection_url = connection_url
+        self.session = None
+        
+    def __enter__(self):
+        engine = create_engine(self.connection_url)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        return self
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.session:
+            self.session.close()
+            
+    # Database operation methods
+    def add(self, obj):
+        self.session.add(obj)
+        self.session.commit()
+        return obj
+    
+    def get(self, model, obj_id: int):
+        return self._session.query(model).get(obj_id)
+
+    def get_by_filter(self, model, return_all: bool = False, **kwargs):
+        if return_all:
+            return self._session.query(model).filter_by(**kwargs).all()
+        else:
+            return self._session.query(model).filter_by(**kwargs).first()
+
+    def update(self, obj_id: int, model, **kwargs):
+        obj = self.get(model, obj_id)
+        for key, value in kwargs.items():
+            setattr(obj, key, value)
+        self._session.commit()
+        return obj
+
+    def delete(self, obj_id: int, model):
+        obj = self.get(model, obj_id)
+        self._session.delete(obj)
+        self._session.commit()
